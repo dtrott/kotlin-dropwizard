b655f97aeedd6d6595c94a047d1f0d0a0a35a7e5
David Trott <github@davidtrott.com>
2022-04-11 21:41:09 -0700

    Introduced Guicey and Hibernate support.
    Updated config.yml to include database connection.
    Modified application init code to also use guice/hibernate.
    Added override to that database fields names are snake cased.

--- src/main/kotlin/com/davidtrott/example/init/ExampleApplication.kt
+++ src/main/kotlin/com/davidtrott/example/init/ExampleApplication.kt
@@ -2,9 +2,16 @@ package com.davidtrott.example.init
 
 import com.davidtrott.example.healthcheck.TemplateHealthCheck
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import com.google.inject.AbstractModule
 import io.dropwizard.Application
+import io.dropwizard.hibernate.ScanningHibernateBundle
 import io.dropwizard.setup.Bootstrap
 import io.dropwizard.setup.Environment
+import org.hibernate.SessionFactory
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy
+import org.hibernate.boot.model.naming.ImplicitNamingStrategyComponentPathImpl
+import org.hibernate.cfg.Configuration
+import ru.vyarus.dropwizard.guice.GuiceBundle
 
 class ExampleApplication : Application<ExampleConfiguration>() {
 
@@ -18,6 +25,31 @@ class ExampleApplication : Application<ExampleConfiguration>() {
     override fun initialize(bootstrap: Bootstrap<ExampleConfiguration>) {
         bootstrap.objectMapper.apply {
             registerKotlinModule()
+        }
+
+        val hibernate =
+            object : ScanningHibernateBundle<ExampleConfiguration>("com.davidtrott.example.database.model") {
+                override fun getDataSourceFactory(configuration: ExampleConfiguration) = configuration.database
+
+                override fun configure(configuration: Configuration) {
+                    super.configure(configuration)
+
+                    // Force snake case on the database fields
+                    configuration.setPhysicalNamingStrategy(CamelCaseToUnderscoresNamingStrategy())
+                }
+            }
+
+        with(bootstrap) {
+            addBundle(hibernate)
+            addBundle(
+                GuiceBundle.builder()
+                    .enableAutoConfig("com.davidtrott.example")
+                    .modules(object : AbstractModule() {
+                        override fun configure() =
+                            bind(SessionFactory::class.java).toInstance(hibernate.sessionFactory)
+                    })
+                    .build()
+            )
         }
     }
 
--- src/main/kotlin/com/davidtrott/example/init/ExampleConfiguration.kt
+++ src/main/kotlin/com/davidtrott/example/init/ExampleConfiguration.kt
@@ -1,9 +1,15 @@
 package com.davidtrott.example.init
 
 import io.dropwizard.Configuration
+import io.dropwizard.db.DataSourceFactory
+import javax.validation.Valid
 import javax.validation.constraints.NotEmpty
 
 data class ExampleConfiguration(
     val template: @NotEmpty String,
     val defaultName: @NotEmpty String = "Stranger",
-) : Configuration()
+) : Configuration() {
+
+    @Valid
+    val database = DataSourceFactory()
+}
